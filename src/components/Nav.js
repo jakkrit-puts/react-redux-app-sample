1cd37eb26a1bbb1860a121db575835609561ced0
Jakkrit Putsorn <jakkrit@super6group.com>
2022-07-21 17:53:21 +0700

    set project

---
+++ src/components/Nav.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from "react-router-dom";
+
+const Nav = () => {
+  return (
+    <div>
+        <h1>Nav</h1>
+    </div>
+  )
+}
+
+export default Nav
---
+++ src/data/products.js
@@ -0,0 +1,27 @@
+export const products = [
+    {
+        id: 1,
+        title: 'Pepsi',
+        price: 15
+    },
+    {
+        id: 2,
+        title: 'Coke',
+        price: 29
+    },
+    {
+        id: 3,
+        title: 'Unif Fruite',
+        price: 18
+    },
+    {
+        id: 4,
+        title: 'Oishi',
+        price: 12
+    },
+    {
+        id: 5,
+        title: 'Fanta',
+        price: 17
+    }
+]
